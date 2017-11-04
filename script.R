411f4111874bb9ba14ccfa7631bb2de7514786ce
François Michonneau <francois.michonneau@gmail.com>
2017-11-04 10:37:31 -0400

    add life expectancy for Canada

--- script.R
+++ script.R
@@ -14,3 +14,10 @@ life_exp_continent <- gapminder %>%
   summarise(
     mean_life_exp = mean(lifeExp)
   )
+
+## Challenge
+## get the life expectancy by year for Canada
+
+life_exp_canada <- gapminder %>% 
+  filter(country == "Canada") %>% 
+  select(year, lifeExp)
